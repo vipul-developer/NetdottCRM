d5bd1b5eb94f467aad9ddf78eb60ab653daa8ede
vipul-developer <vipul.sharma234@gmail.com>
2021-05-13 01:05:54 +0530

    React App Project Init 12/05/2021 With Routes And Home And Layout

--- client/src/Components/Public/Home/index.js
+++ client/src/Components/Public/Home/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { connect } from "react-redux";
 import { Link,withRouter } from "react-router-dom";
 import { Paper,Grid,Container,Typography } from "@material-ui/core";
+import Visitor from "../../../Resources/Images/visitor.jpg";
 const Home = () => {
     return (
         <div>
             <Grid item xs={12} container>
-                <Paper>
-                    <Container fixed>
-                        <Typography>Home</Typography>
-                    </Container>
-                </Paper>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+                        <Paper className="p-5 text-center" square={true} elevation={0}>
+                            <Typography>Home</Typography>
+                        </Paper>
+                </Grid>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+                    <Paper className="VisitorBck" style={{backgroundImage:`url(${Visitor})`}}></Paper>
+                </Grid>
             </Grid>
         </div>
     );
