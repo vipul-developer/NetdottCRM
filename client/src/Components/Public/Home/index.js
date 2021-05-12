79c03e06e7e9b46a9887e72a069d27b6583e448c
vipul-developer <vipul.sharma234@gmail.com>
2021-05-13 01:27:24 +0530

    React App Project Init 12/05/2021 With Routes And Home And Layout

--- client/src/Components/Public/Home/index.js
+++ client/src/Components/Public/Home/index.js
@@ -7,13 +7,18 @@ const Home = () => {
     return (
         <div>
             <Grid item xs={12} container>
-                <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
-                        <Paper className="p-5 text-center" square={true} elevation={0}>
-                            <Typography>Home</Typography>
+                <Grid item xl={6} lg={6} md={6} sm={6} xs={12} container>
+                    <Grid item xl={2} lg={2} md={2} sm={2}/>
+                    <Grid item xl={8} lg={8} md={8} sm={8} xs={12}>
+                        <Paper className="pt-5 mt-5" square={true} elevation={0}>
+                            <Typography variant="h4" gutterBottom>Login to <strong>Net-Dott</strong></Typography>
+                            <Typography className="mb-4" variant="body1">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</Typography>
                         </Paper>
+                    </Grid>
+                    <Grid item xl={2} lg={2} md={2} sm={2}/>
                 </Grid>
                 <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
-                    <Paper className="VisitorBck" style={{backgroundImage:`url(${Visitor})`}}></Paper>
+                    <Paper className="VisitorBck" square={true} elevation={0} style={{backgroundImage:`url(${Visitor})`}}></Paper>
                 </Grid>
             </Grid>
         </div>
